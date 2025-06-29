b94464311d7c9aaccb3a9dc3c7dde392c217f83d
Petri-Johan Last <petri.last@gmail.com>
2025-06-29 08:05:35 +0200

    Replace google-chrome with librewolf

--- flake.nix
+++ flake.nix
@@ -156,32 +156,11 @@
               "anki"
               "ghostty"
               "gimp"
-              "google-chrome"
+	      "librewolf"
               "whatsapp"
               "zoom"
             ];
           };
-
-          system.activationScripts.applications.text =
-            let
-              env = pkgs.buildEnv {
-                name = "system-applications";
-                paths = config.environment.systemPackages;
-                pathsToLink = "/Applications";
-              };
-            in
-            pkgs.lib.mkForce ''
-              	  # Set up applications.
-              	  echo "setting up /Applications..." >&2
-              	  rm -rf /Applications/Nix\ Apps
-              	  mkdir -p /Applications/Nix\ Apps
-              	  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-              	  while read -r src; do
-              	    app_name=$(basename "$src")
-              	    echo "copying $src" >&2
-              	    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-              	  done
-              	'';
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
